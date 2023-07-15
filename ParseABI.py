d4615b1161df46c8686fd644a770438f4940a514
SkullElf <120666731+SkullElf@users.noreply.github.com>
2023-07-15 11:28:06 +0300

    Add files via upload

--- ParseABI.py
+++ ParseABI.py
@@ -1,8 +1,9 @@
 import base64
 import aiohttp
 from multiversx_sdk_core import Address
-
+import json
 from config import PROXY_URL, SIZE_PER_TYPE
+from TypeParser import ABITypeParser
 
 
 def int_to_hex(number):
@@ -13,12 +14,26 @@ def int_to_hex(number):
 def convert_args(args):
     args_output = []
     for arg in args:
-        if arg["type"] in list(SIZE_PER_TYPE.keys()):
-            args_output.append(int_to_hex(arg["value"]))
-        elif arg["type"] == "Address":
-            args_output.append(Address.from_bech32(arg["value"]).hex())
+        print(arg)
+        if arg["type"].startswith("variadic<"):
+            arg["type"] = arg["type"].replace("variadic<", "")[:-1]
+        if "<" in arg["type"]:
+            arg["type"] = arg["type"].split("<")[1].replace(">", "")
+        if ',' in arg["value"]:
+            for piece in arg["value"].split(','):
+                if arg["type"] in list(SIZE_PER_TYPE.keys()):
+                    args_output.append(int_to_hex(piece))
+                elif arg["type"] == "Address":
+                    args_output.append(Address.from_bech32(piece).hex())
+                else:
+                    args_output.append(piece.encode('ascii').hex())
         else:
-            args_output.append(arg["value"].encode('ascii').hex())
+            if arg["type"] in list(SIZE_PER_TYPE.keys()):
+                args_output.append(int_to_hex(arg["value"]))
+            elif arg["type"] == "Address":
+                args_output.append(Address.from_bech32(arg["value"]).hex())
+            else:
+                args_output.append(arg["value"].encode('ascii').hex())
     return args_output
 
 
@@ -46,112 +61,22 @@ async def query_sc(endpoint, sc_address, args=None):
     async with aiohttp.ClientSession() as session:
         async with session.post(url, json=body) as response:
             response_json = await response.json()
+            if response_json["data"]["data"]["returnCode"] != "ok":
+                return None, response_json["data"]["data"]["returnMessage"]
             return response_json["data"]["data"]["returnData"]
 
 
-def read_hex(data, object_type, types):
-    if data.hex() == "":
-        return None, 0
-    parsed_data = {}
-    offset = 0
-    if object_type in (["BigUint", "Address", "bool", "TokenIdentifier", "EgldOrEsdtTokenIdentifier"]) + list(SIZE_PER_TYPE.keys()):
-        result = None
-        if object_type in list(SIZE_PER_TYPE.keys()):
-            result = int.from_bytes(data[offset:offset + SIZE_PER_TYPE.get(object_type)], byteorder="big")
-            offset += SIZE_PER_TYPE.get(object_type)
-        elif object_type == "bool":
-            result = data[offset] == 1
-            offset += 1
-        elif object_type in ["TokenIdentifier", "EgldOrEsdtTokenIdentifier"]:
-            result = data.decode()
-            offset += 4
-        elif object_type in ["BigUint"]:
-            obj_len = int.from_bytes(data[offset:offset + 4], byteorder="big")
-            offset += 4
-            result = str(int.from_bytes(data[offset:offset + obj_len], byteorder="big"))
-            offset += obj_len
-        elif object_type == "Address":
-            hex_address = data[offset:offset + 32].hex()
-            result = Address.from_hex(hex_address, hrp="erd").bech32()
-            offset += 32
-        return result, offset
-
-    if object_type not in types or types[object_type]["type"] == "struct":
-        for field in types[object_type]["fields"]:
-            field_name = field["name"]
-            field_type = field["type"]
-            if field_type.startswith("List<"):
-                list_length = int.from_bytes(data[offset:offset + 4], byteorder="big")
-                offset += 4
-                subtype = field_type.replace("List<", "").replace(">", "")
-                parsed_data[field_name] = []
-                for index in range(list_length):
-                    parsed_item, offset1 = read_hex(data[offset:], subtype, types)
-                    offset += offset1
-                    parsed_data.get(field_name).append(parsed_item)
-            if field_type.startswith("Option<"):
-                if data[offset] == 0:
-                    offset += 1
-                    continue
-                offset += 1
-                field_type = field_type.replace("Option<", '').replace(">", "")
-            if field_type in ["BigUint", "Address", "bool", "TokenIdentifier", "EgldOrEsdtTokenIdentifier"] + list(SIZE_PER_TYPE.keys()):
-                if field_type == "bool":
-                    parsed_data[field_name] = bool(int(data[offset], 16))
-                    offset += 1
-                elif object_type in list(SIZE_PER_TYPE.keys()):
-                    parsed_data[field_name] = int.from_bytes(data[offset:offset + SIZE_PER_TYPE.get(object_type)], byteorder="big")
-                    offset += SIZE_PER_TYPE.get(object_type)
-                elif field_type in ["TokenIdentifier", "EgldOrEsdtTokenIdentifier"]:
-                    obj_len = int.from_bytes(data[offset:offset + 4], byteorder="big")
-                    offset += 4
-                    parsed_data[field_name] = data[offset:offset + obj_len].decode('ascii')
-                    offset += obj_len
-                elif field_type in ["BigUint"]:
-                    obj_len = int.from_bytes(data[offset:offset + 4], byteorder="big")
-                    offset += 4
-                    parsed_data[field_name] = str(int.from_bytes(data[offset:offset + obj_len], byteorder="big"))
-                    offset += obj_len
-                elif field_type == "Address":
-                    hex_address = data[offset:offset + 32].hex()
-                    parsed_data[field_name] = Address.from_hex(hex_address, hrp="erd").bech32()
-                    offset += 32
-            elif field_type in types:
-                parsed_data[field_name], offset1 = read_hex(data[offset:], field_type, types)
-                offset += offset1
-    elif types[object_type]["type"] == "enum":
-        for item in types[object_type]["variants"]:
-            if data[offset] == item["discriminant"]:
-                offset += 1
-                return item["name"], offset
-    return parsed_data, offset
-
-
-async def parse_abi(sc_address, func, endpoints, types, args=None):
+async def parse_abi(sc_address, func, endpoints, abi_json, args=None):
     if args is None:
         args = []
     endpoint_data = next((d for d in endpoints if d['name'] == func), None)
     if endpoint_data is None:
         return None
     answer = await query_sc(func, sc_address, args=args)
-    return_data_type = endpoint_data["outputs"][0]["type"]
-
-    if return_data_type.startswith("variadic<"):
-        return_data_type = return_data_type.replace("variadic<", "")[:-1]
-    if "<" in return_data_type:
-        return_data_type = return_data_type.split("<")[1].replace(">", "")
+    if isinstance(answer, tuple):
+        return 400, answer[1]
     decoded_answer = decode_return_data(answer)
-    if decoded_answer is None:
-        return None
-    output = []
-    for l1 in decoded_answer:
-        if ',' in return_data_type:
-            tuple_output = []
-            for single_type in return_data_type.split(','):
-                data_output, _ = read_hex(l1, single_type, types)
-                tuple_output.append(data_output)
-            data_output = tuple(tuple_output)
-        else:
-            data_output, _ = read_hex(l1, return_data_type, types)
-        output.append(data_output)
-    return output
+    abi_type_parser = ABITypeParser(abi_json)
+    response_type = endpoint_data["outputs"][0]["type"]
+    parsed_data = abi_type_parser.parse_hex_response(decoded_answer, response_type)
+    return 200, parsed_data
---
+++ TypeParser.py
@@ -0,0 +1,166 @@
+from typing import Any, List, Dict, Optional, Tuple
+from multiversx_sdk_core import Address
+
+
+class ABITypeParser:
+    def __init__(self, abi_json: Dict[str, Any]) -> None:
+        self.types: Dict[str, Any] = {}
+        if "types" in abi_json:
+            types = abi_json["types"]
+            for type_name, type_value in types.items():
+                self.types[type_name] = type_value
+
+    def parse_hex_response(self, hex_responses: list, response_type: str) -> Any:
+        result = []
+        for hex_response in hex_responses:
+            parsed_data, _ = self.read_hex(hex_response, response_type)
+            result.append(parsed_data)
+        if len(result) == 1:
+            return result[0]
+        return result
+
+    def read_hex(self, data: bytes, object_type: str) -> Tuple[Any, int]:
+        if object_type in ["u8", "i8", "u16", "i16", "u32", "i32", "u64", "i64", "bool", "TokenIdentifier",
+                           "EgldOrEsdtTokenIdentifier", "BigUint", "bytes"]:
+            return self.read_primitive_type(data, object_type)
+        elif object_type == "Address":
+            return self.read_address_type(data)
+        elif object_type.startswith("List<"):
+            subtype = object_type.replace("List<", "")[:-1]
+            return self.read_list_type(data, subtype)
+        elif object_type.startswith("optional<"):
+            subtype = object_type.replace("optional<", "")[:-1]
+            return self.read_hex(data, subtype)
+        elif object_type.startswith("variadic<"):
+            subtype = object_type.replace("variadic<", "")[:-1]
+            return self.read_hex(data, subtype)
+        elif object_type.startswith("Option<"):
+            subtype = object_type.replace("Option<", "")[:-1]
+            return self.read_option_type(data, subtype)
+        elif object_type.startswith("multi<"):
+            subtypes = object_type.replace("multi<", "")[:-1].split(",")
+            return self.read_multi_type(data, subtypes)
+        elif object_type in self.types:
+            fields = self.types[object_type]
+            if isinstance(fields, dict) and fields.get("type") == "enum":
+                return self.read_enum_type(data, fields)
+            elif isinstance(fields, dict) and fields.get("type") == "struct":
+                struct_fields = fields["fields"]
+                parsed_object = {}
+                offset = 0
+                for field in struct_fields:
+                    field_name = field["name"]
+                    field_type = field["type"]
+                    parsed_field, field_length = self.read_hex(data[offset:], field_type)
+                    parsed_object[field_name] = parsed_field
+                    offset += field_length
+                return parsed_object, offset
+            elif isinstance(fields, list):
+                # Handle tuple type
+                parsed_object = []
+                offset = 0
+                for field_type in fields:
+                    parsed_field, field_length = self.read_hex(data[offset:], field_type)
+                    parsed_object.append(parsed_field)
+                    offset += field_length
+                return tuple(parsed_object), offset
+        else:
+            raise ValueError(f"Unsupported type: {object_type}")
+
+    def read_multi_type(self, data: bytes, subtypes: List[str]) -> Tuple[Tuple[Any, ...], int]:
+        parsed_items = []
+        offset = 0
+        for subtype in subtypes:
+            parsed_item, item_length = self.read_hex(data[offset:], subtype)
+            parsed_items.append(parsed_item)
+            offset += item_length
+        return tuple(parsed_items), offset
+
+    def read_enum_type(self, data: bytes, enum_fields: Dict[str, Any]) -> Tuple[Any, int]:
+        variants = enum_fields.get("variants", [])
+        discriminant_data = data[0]
+        discriminant = discriminant_data
+        variant = variants[discriminant]
+        item_length = 1
+        return variant["name"], item_length
+
+    def read_primitive_type(self, data: bytes, object_type: str) -> Tuple[Any, int]:
+        if object_type == "bytes":
+            return data, len(data)
+        elif object_type in ["u8", "i8"]:
+            parsed_item = int.from_bytes(data[:1], byteorder="big")
+            item_length = 1
+        elif object_type in ["u16", "i16"]:
+            parsed_item = int.from_bytes(data[:2], byteorder="big")
+            item_length = 2
+        elif object_type in ["u32", "i32"]:
+            parsed_item = int.from_bytes(data[:4], byteorder="big")
+            item_length = 4
+        elif object_type in ["u64", "i64"]:
+            parsed_item = int.from_bytes(data[:8], byteorder="big")
+            item_length = 8
+        elif object_type == "bool":
+            parsed_item = bool(int.from_bytes(data[:1], byteorder="big"))
+            item_length = 1
+        elif object_type == "TokenIdentifier":
+            parsed_item, item_length = self.read_token_identifier(data)
+        elif object_type == "EgldOrEsdtTokenIdentifier":
+            parsed_item, item_length = self.read_egld_or_esdt_token_identifier(data)
+        elif object_type == "BigUint":
+            obj_len = int.from_bytes(data[:4], byteorder="big")
+            item_length = 4
+            parsed_item = int.from_bytes(data[item_length:item_length + obj_len], byteorder="big")
+            item_length += obj_len
+        else:
+            raise ValueError(f"Unsupported primitive type: {object_type}")
+        return parsed_item, item_length
+
+    def read_token_identifier(self, data: bytes) -> Tuple[str, int]:
+        if not data.hex().startswith("0000"):
+            return data.decode('ascii'), len(data)
+        obj_len = int.from_bytes(data[:4], byteorder="big")
+        item_length = 4
+        parsed_item = data[item_length:item_length + obj_len].decode("ascii")
+        item_length += obj_len
+        return parsed_item, item_length
+
+    def read_egld_or_esdt_token_identifier(self, data: bytes) -> Tuple[str, int]:
+        if not data.hex().startswith("0000"):
+            return data.decode('ascii'), len(data)
+        obj_len = int.from_bytes(data[:4], byteorder="big")
+        item_length = 4
+        parsed_item = data[item_length:item_length + obj_len].decode("ascii")
+        item_length += obj_len
+        return parsed_item, item_length
+
+    def read_address_type(self, data: bytes) -> Tuple[str, int]:
+        hex_address = data[:32].hex()
+        parsed_item = Address.from_hex(hex_address, hrp="erd").bech32()
+        item_length = 32
+        return parsed_item, item_length
+
+    def read_list_type(self, data: bytes, subtype: str) -> Tuple[List[Any], int]:
+        parsed_list = []
+        offset = 0
+        while offset < len(data):
+            parsed_item, item_length = self.read_hex(data[offset:], subtype)
+            parsed_list.append(parsed_item)
+            offset += item_length
+            if offset < len(data) and data[offset] == 1:
+                offset += 1
+            else:
+                break
+
+        return parsed_list, offset
+
+    def read_option_type(self, data: bytes, subtype: str) -> Tuple[Optional[Any], int]:
+        if len(data) == 0:
+            return None, 0
+        presence_flag = data[0]
+        offset = 1
+        if presence_flag == 0:
+            return None, offset
+        parsed_item, item_length = self.read_hex(data[offset:], subtype)
+        offset += item_length
+        return parsed_item, offset
+
